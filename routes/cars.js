0d9118fd37c98ffb7e3b5204f9e5f9e38035fbb7
Affan Bin Faisal <afaisal.bese21seecs@seecs.edu.pk>
2024-02-03 16:45:07 +0500

    Auth and API

--- routes/cars.js
+++ routes/cars.js
@@ -3,12 +3,15 @@ const axios = require("axios");
 const router = express.Router();
 const User = require("./../models/User");
 
+const apiToken = "3e23a1f48c83bf8ac31386723c4f719c60123b1e37298ec2ec24835e1af2922e";
+
 router.get("/:vin", async (req, res) => {
   // 5UXKR0C52G0P21189
-  const vinNumber = req.params.vin;
   try {
-    const carsResponse = await axios.post("https://copart-iaai-api.com/api/v1/get-car-vin", {
-      api_token: "3e23a1f48c83bf8ac31386723c4f719c60123b1e37298ec2ec24835e1af2922e",
+    const vinNumber = req.params.vin;
+    const url = "https://copart-iaai-api.com/api/v1/get-car-vin";
+    const carsResponse = await axios.post(url, {
+      api_token: apiToken,
       vin_number: vinNumber,
     });
 
@@ -19,6 +22,24 @@ router.get("/:vin", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const apiUrl = `https://copart-iaai-api.com/api/v2/get-cars`;
+    const response = await axios.post(apiUrl, {
+      params: {
+        api_token: apiToken,
+        ...req.query,
+      },
+    });
+
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
--- routes/login.js
+++ routes/login.js
@@ -7,22 +7,26 @@ const router = express.Router();
 const secretKey = 'your-secret-key';
 
 router.post('/', async (req, res) => {
-  const { username, password } = req.body;
-  const usersList = await User.find({ username: username });
-  if (usersList.length > 0) {
-    const user = usersList[0];
-    if (await bcrypt.compare(password, user.password)) {
-      const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
-      res.status(200).json({ token });
-    }
-    else {
-      res.status(401).json({ message: "Incorrect username or password" });
-    }
+  try {
+    const { username, password } = req.body;
+    const usersList = await User.find({ username: username });
 
-  } else {
-    res.status(401).json({ message: "User not found" });
+    if (usersList.length > 0) {
+      const user = usersList[0];
+
+      if (await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ error: "Incorrect username or password" });
+      }
+    } else {
+      res.status(401).json({ error: "User not found" });
+    }
+  } catch (error) {
+    console.error('Error during authentication:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-
 });
 
 module.exports = router;
